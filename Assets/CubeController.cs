d04da534f5cab13bcec115405e7de81d491ee84a
MTA-ui <73399838+MTA-ui@users.noreply.github.com>
2021-01-28 22:33:16 +0900

    Initial commit

---
+++ Assets/CubeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+
+    // キューブの移動速度
+    private float speed = -12;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キューブを移動させる
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "cubeTag" || col.gameObject.tag == "groundTag")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+}
